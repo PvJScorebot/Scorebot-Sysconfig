975a2fbc7a61f5c9124462f07e5f522b62d3fac9
iDigitalFlame <idf@idigitalflame.com>
2019-07-03 22:54:38 -0400

    Updated setup config file.

--- setup.sh
+++ setup.sh
@@ -1,154 +1,181 @@
 #!/usr/bin/bash
 
-printf "[?] Scorebot Setup v1\n"
+SCOREBOT_BRANCH=""
+SCOREBOT_DIR="/opt/scorebot"
+SCOREBOT_URL="https://github.com/iDigitalFlame/scorebot-core"
 
-printf "[?] IP Address with Slash: "
-read address
+SYSCONFIG_DIR="/opt/sysconfig"
+SYSCONFIG_URL="https://github.com/iDigitalFlame/scorebot-sysconfig"
 
-printf "[?] Gateway: "
-read gateway
+log() {
+    if [ $# -ne 1 ]; then
+        return 0
+    fi
+    printf "[+] $1\n"
+}
+run() {
+    if [ $# -ne 1 ]; then
+        return 0
+    fi
+    bash -c "$1; exit \$?"
+    if [ $? -ne 0 ]; then
+        printf '[!] Command \"$1\" did not exit with zero, quitting!\n'
+        exit 1
+    fi
+    return 1
+}
 
-printf "[?] DNS 1: "
-read dns1
+setup() {
+    log "Updating system.."
+    run "pacman -Syy" 1> /dev/null
+    run "pacman -Syu --noconfirm"
+    log "Installing required packages.."
+    run "pacman -S git net-tools pacman-contrib --noconfirm"
+    run "Downloading sysconfig base from github.."
+    if [ -d "$SYSCONFIG_DIR" ]; then
+        mv "$SYSCONFIG_DIR" "${SYSCONFIG_DIR}.old"
+    fi
+    run "git clone \"$SYSCONFIG_URL\" \"$SYSCONFIG_DIR\""
+    printf "SYSCONFIG={$SYSCONFIG_DIR}\n" > "/etc/sysconfig.conf"
+    chmod 444 "/etc/sysconfig.conf"
+    log "Initilizing sysconfig.."
+    run "bash ${SYSCONFIG_DIR}/bin/relink / 2> /dev/null"
+    run "bash ${SYSCONFIG_DIR}/bin/syslink > /dev/null"
+    run "syslink 1> /dev/null 2> /dev/null"
+    log "Enabling required services.."
+    run "systemctl enable sshd.service > /dev/null"
+    run "systemctl enable fstrim.timer  > /dev/null"
+    run "systemctl enable checkupdates.timer > /dev/null"
+    run "systemctl enable checkupdates.service > /dev/null"
+    run "systemctl enable reflector.timer > /dev/null"
+    run "systemctl enable reflector.service > /dev/null"
+    log "Finished basic setup.."
+}
+setup_db() {
+    db_root_pw=""
+    db_scorebot_pw=""
+    db_scorebot_ip=""
+    while [ -z "$db_root_pw" ] || [ -z "$db_scorebot_pw" ] || [ -z "$db_scorebot_ip "]; do
+        printf "MySQL root password? "
+        read db_root_pw
+        printf "MySQL scorebot password? "
+        read db_scorebot_pw
+        printf "Scorebot API IP Address? "
+        read db_scorebot_ip
+    done
+    log "Scorebot IP is \"$db_scorebot_ip\", this can be changed in the \"/etc/hosts\" file.."
+    printf "scorebot-database" > "${SYSCONFIG_DIR}/etc/hostname"
+    printf "$db_scorebot_ip\tscorebot-core\n" >> "${SYSCONFIG_DIR}/etc/hosts"
+    log "Installing database dependencies.."
+    run "pacman -S mariadb --noconfirm"
+    log "Installing inital database.."
+    run "mysql_install_db --basedir=/usr --ldata=/var/lib/mysql --user=mysql 1> /dev/null"
+    run "systemctl enable mysqld > /dev/null"
+    run "systemctl start mysqld > /dev/null"
+    log "Securing database.."
+    run "mysql -u root -e \"DELETE FROM mysql.user WHERE User='';\""
+    run "mysql -u root -e \"DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');\""
+    run "mysql -u root -e \"DROP DATABASE IF EXISTS test;\""
+    run "mysql -u root -e \"DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';\""
+    run "mysql -u root -e \"FLUSH PRIVILEGES;\""
+    run "mysql -u root -e \"CREATE DATABASE scorebot_db;\""
+    run "mysql -u root -e \"GRANT ALL ON scorebot_db.* TO 'scorebot'@'scorebot-core' IDENTIFIED BY '$db_scorebot_pw';\""
+    run "mysql -u root -e \"SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$db_root_pw')\""
+    run "systemctl restart mysqld 1> /dev/null"
+    log "Database setup complete, please configure the core component to use the supplied password!"
+}
+setup_core() {
+    core_db_pw=""
+    core_db_ip=""
+    core_django_pw=""
+    while [ -z "$core_db_pw" ] || [ -z "$core_django_pw" ] || [ -z "$core_db_ip "]; do
+        printf "Django root password? "
+        read core_django_pw
+        printf "MySQL scorebot password? "
+        read core_db_pw
+        printf "MySQL Server IP Address? "
+        read core_db_ip
+    done
+    printf "scorebot-core" > "${SYSCONFIG_DIR}/etc/hostname"
+    printf "$proxy_scorcore_db_ipebot_ip\tscorebot-database\n" >> "${SYSCONFIG_DIR}/etc/hosts"
+    log "MySQL Server IP is \"$core_db_ip\", this can be changed in the \"/etc/hosts\" file.."
+    log "Installing core dependencies.."
+    run "pacman -S apache mod_wsgi python python-pip python-virtualenv python-django gcc mariadb-clients python-mysqlclient --noconfirm"
+    run "mkdir -p \"${SCOREBOT_DIR}/versions\""
+    log "Building virtual env.."
+    run "virtualenv --always-copy \"${SCOREBOT_DIR}/python\" 1> /dev/null"
+    run "git clone \"$SCOREBOT_URL\" \"${SCOREBOT_DIR}/version/release\""
+    if ! [ -z "$SCOREBOT_BRANCH" ]; then
+        run "cd \"${SCOREBOT_DIR}/version/release\"; git checkout $SCOREBOT_BRANCH"
+    fi
+    run "ln -s \"${SCOREBOT_DIR}/version/release\" \"${SCOREBOT_DIR}/current\""
+    log "Installing PIP requirements.."
+    run "source \"${SCOREBOT_DIR}/python/bin/activate\"; cd \"${SCOREBOT_DIR}/current\"; unset PIP_USER; pip install -r requirements.txt 1> /dev/null"
+    run "sed -ie 's/\"PASSWORD\": \"password\",/\"PASSWORD\": \"$core_db_pw\",/g' \"${SCOREBOT_DIR}/current/scorebot/settings.py\""
+    run "rm ${SCOREBOT_DIR}/current/scorebot/*e"
+    log "Attempting to push migrations to database server \"$core_db_ip\".."
+    run "source \"${SCOREBOT_DIR}/python/bin/activate\"; cd \"${SCOREBOT_DIR}/current\"; env SBE_SQLLITE=0 python manage.py makemigrations scorebot_grid scorebot_core scorebot_game"
+    run "source \"${SCOREBOT_DIR}/python/bin/activate\"; cd \"${SCOREBOT_DIR}/current\"; env SBE_SQLLITE=0 python manage.py migrate"
+    run "source \"${SCOREBOT_DIR}/python/bin/activate\"; cd \"${SCOREBOT_DIR}/current\"; env SBE_SQLLITE=0 python manage.py shell -c \"from django.contrib.auth.models import User; User.objects.create_superuser('root', '', '$core_django_pw')\""
+    log "Created Django admin account \"root\" with supplied password!"
+    run "ln -s \"${SYSCONFIG_DIR}/etc/httpd/conf/roles/core.conf\" \"/etc/httpd/conf/scorebot-role.conf\""
+    run "ln -s /usr/lib/python3.*/site-packages/django/contrib/admin/static/admin \"${SCOREBOT_DIR}/current/scorebot_static/admin\""
+    run "chown root:http -R \"${SCOREBOT_DIR}\""
+    run "chmod 550 -R \"${SCOREBOT_DIR}/currebt\""
+    run "mkdir -p \"${SCOREBOT_DIR}/current/scorebot_media\""
+    run "chown http:http \"${SCOREBOT_DIR}/current/scorebot_media\""
+    run "chmod 775 \"${SCOREBOT_DIR}/current/scorebot_media\""
+    run "systemctl enable httpd.service > /dev/null"
+    run "systemctl enable scorebot.service > /dev/null"
+    run "systemctl start httpd.service > /dev/null"
+    run "systemctl start scorebot.service > /dev/null"
+    log "Core setup complete!"
+}
+setup_proxy() {
+    proxy_scorebot_ip=""
+    while [ -z "$proxy_scorebot_ip" ]; do
+        printf "Scorebot API IP Address? "
+        read proxy_scorebot_ip
+    done
+    printf "scorebot-proxy" > "${SYSCONFIG_DIR}/etc/hostname"
+    printf "$proxy_scorebot_ip\tscorebot-core\n" >> "${SYSCONFIG_DIR}/etc/hosts"
+    log "Scorebot IP is \"$proxy_scorebot_ip\", this can be changed in the \"/etc/hosts\" file.."
+    log "Installing proxy dependencies.."
+    run "pacman -S apache --noconfirm"
+    log "Enabling and starting Apache proxy..."
+    run "ln -s \"${SYSCONFIG_DIR}/etc/httpd/conf/roles/proxy.conf\" \"/etc/httpd/conf/scorebot-role.conf\" 1> /dev/null"
+    run "systemctl enable httpd.service > /dev/null"
+    run "systemctl start httpd.service > /dev/null"
+    log "Proxy setup complete, please ensure to configure the core component!"
+}
 
-printf "[?] DNS 2: "
-read dns2
+log "Scorebot Setup v2"
+log "iDigitalFlame, The Scorebot Project 2019"
 
-if [ -z $address ] || [ -z $gateway ] || [ -z $dns1 ] || [ -z $dns2 ]; then
-    printf "[!] Invalid network settings! Quitting!\n"
-    exit 1
-fi
+log "Select the role for this server.."
+printf "[?] Roles:\n\t1: Core\n\t2: DB\n\t3: Proxy\nChoice [1-3]: "
+read sbe_role
 
-printf "[?] Role (0 - Core, 1 - DB, 2 - Proxy) [0-2]: "
-read role
-
-packages=""
-case $role in
-    0)
-    packages="apache mod_wsgi python python-pip python-virtualenv python-django gcc mariadb-clients python-mysqlclient"
-    ;;
+case $sbe_role in
     1)
-    packages="mariadb"
+    setup
+    setup_core
     ;;
     2)
-    packages="apache"
+    setup
+    setup_db
+    ;;
+    3)
+    setup
+    setup_proxy
     ;;
     *)
-    printf "[!] Invalid role! Quitting\n"
-    exit 1
+    log "Invalid role selected! Please try again..\nGoodbye"
+    exit 255
     ;;
 esac
 
-printf "[+] Updating system....\n"
-pacman -Syu --noconfirm
-printf "[+] Installing required packages..\n"
-pacman -S git net-tools pacman-contrib --noconfirm
-pacman -S $packages --noconfirm
-
-if [ $role -eq 0 ] || [ $role -eq 2 ]; then
-    rm -rf /etc/httpd/conf/extra
-    rm /etc/httpd/conf/httpd.conf
-fi
-
-if [ -d /opt/sysconfig ]; then
-    mv /opt/sysconfig /opt/sysconfig.bak
-fi
-
-git clone https://github.com/iDigitalFlame/scorebot-sysconfig /opt/sysconfig
-printf "SYSCONFIG=/opt/sysconfig\n" > /etc/sysconfig.conf
-chmod 444 /etc/sysconfig.conf
-
-mkdir -p /opt/sysconfig/etc/udev.d/rules.d
-mkdir -p /opt/sysconfig/etc/systemd/network
-mac=$(ifconfig | grep ether | awk '{print $2}')
-printf "[Match]\nName=en0\n\n[Network]Address=$address\nDNS=$dns1\nDNS=dns$2\n\n[Route]\nGateway=$gateway\n" > /opt/sysconfig/etc/systemd/network/en0.network
-printf "SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$mac\", NAME=\"en0\"" > /opt/sysconfig/etc/udev.d/rules.d/10-network.rules
-
-bash /opt/sysconfig/bin/relink /opt/sysconfig / 2> /dev/null
-bash /opt/sysconfig/bin/syslink 1> /dev/null 2> /dev/null
-syslink 1> /dev/null 2> /dev/null
-
-systemctl enable sshd.service 1> /dev/null
-systemctl enable fstrim.timer  1> /dev/null
-systemctl enable checkupdates.timer 1> /dev/null
-systemctl enable checkupdates.service 1> /dev/null
-systemctl enable reflector.timer 1> /dev/null
-systemctl enable reflector.service 1> /dev/null
-
-if [ $role -eq 0 ]; then
-    printf "scorebot-core" > /opt/sysconfig/etc/hostname
-    syslink 1> /dev/null
-    mkdir /opt/scorebot/versions -p
-    printf "[+] Building virtual env...\n"
-    virtualenv --always-copy /opt/scorebot/python 1> /dev/null
-    git clone https://github.com/iDigitalFlame/scorebot-core /opt/scorebot/version/release
-    ln -s /opt/scorebot/version/release /opt/scorebot/current
-    printf "[+] Installing PIP requirements..\n"
-    bash -c "source /opt/scorebot/python/bin/activate; cd /opt/scorebot/current; unset PIP_USER; pip install -r requirements.txt" 1> /dev/null
-    printf "[?] Databse server IP? "
-    read sbe_db
-    printf "[?] Databse 'Scorebot' password? "
-    read sbe_db_pass
-    sed -ie "s/\"PASSWORD\": \"password\",/\"PASSWORD\": \"$sbe_db_pass\",/g" /opt/scorebot/current/scorebot/settings.py
-    rm /opt/scorebot/current/scorebot/*e
-    printf "127.0.0.1 scorebot\n$sbe_db scorebot-db\n" > /opt/sysconfig/etc/hosts
-    syslink
-    bash -c "source /opt/scorebot/python/bin/activate; cd /opt/scorebot/current; env SBE_SQLLITE=0 python manage.py makemigrations scorebot_grid scorebot_core scorebot_game"
-    bash -c "source /opt/scorebot/python/bin/activate; cd /opt/scorebot/current; env SBE_SQLLITE=0 python manage.py migrate"
-    printf "[?] Django superuser password? "
-    read db_pass
-    bash -c "source /opt/scorebot/python/bin/activate; cd /opt/scorebot/current; env SBE_SQLLITE=0 python manage.py shell -c \"from django.contrib.auth.models import User; User.objects.create_superuser('root', '', '$db_pass')\""
-    printf "[+] Created default account \"root\" with supplied password!\n"
-    ln -s /opt/sysconfig/etc/httpd/conf/roles/core.conf /etc/httpd/conf/scorebot-role.conf
-    ln -s /usr/lib/python3.*/site-packages/django/contrib/admin/static/admin /opt/scorebot/current/scorebot_static/admin
-    chown root:http -R /opt/scorebot/
-    chmod 550 -R /opt/scorebot/current/
-    mkdir -p /opt/scorebot/current/scorebot_media
-    chown http:http /opt/scorebot/current/scorebot_media
-    chmod 775 /opt/scorebot/current/scorebot_media
-    systemctl enable httpd.service 1> /dev/null
-    systemctl enable scorebot.service 1> /dev/null
-    systemctl start httpd.service 1> /dev/null
-    systemctl start scorebot.service 1> /dev/null
-fi
-
-if [ $role -eq 1 ]; then
-    printf "scorebot-db" > /opt/sysconfig/etc/hostname
-    syslink
-    mysql_install_db --basedir=/usr --ldata=/var/lib/mysql --user=mysql
-    systemctl enable mysqld 1> /dev/null
-    systemctl start mysqld 1> /dev/null
-    mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
-    mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-    mysql -u root -e "DROP DATABASE IF EXISTS test;"
-    mysql -u root -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
-    mysql -u root -e "FLUSH PRIVILEGES;"
-    mysql -u root -e "CREATE DATABASE scorebot_db;"
-    printf "[?] Scorebot user password? "
-    read sbe_db_pass
-    printf "[?] Scorebot server IP? "
-    read sbe_core
-    printf "127.0.0.1 scorebot-db\n$sbe_core scorebot-core\n" > /opt/sysconfig/etc/hosts
-    syslink
-    mysql -u root -e "GRANT ALL ON scorebot_db.* TO 'scorebot'@'scorebot-core' IDENTIFIED BY '$sbe_db_pass';"
-    printf "[?] Mysql root password? "
-    read django_pass
-    mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('$django_pass') WHERE User='root';"
-    printf "[+] Created default account \"root\" with supplied password!\n"
-    systemctl restart mysqld 1> /dev/null
-fi
-
-if [ $role -eq 2 ]; then
-    printf "scorebot-proxy" > /opt/sysconfig/etc/hostname
-    syslink
-    printf "Scorebot IP to proxy? "
-    read sbe_core
-    ln -s /opt/sysconfig/etc/httpd/conf/roles/proxy.conf /etc/httpd/conf/scorebot-role.conf
-    printf "127.0.0.1 scorebotproxy\n$sbe_core scorebot-core\n" > /opt/sysconfig/etc/hosts
-    syslink
-    systemctl enable httpd.service 1> /dev/null
-    systemctl start httpd.service 1> /dev/null
-fi
-
-syslink
-printf "[+] Done\n"
+log "Finilizing with a syslink.."
+run "syslink > /dev/null"
+log "Done\nHave Fun!"
+exit 0
